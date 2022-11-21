18cfbeaa8a4ba2c04ac2e4b4b323809a065e2294
stqc <prateekdang54@gmail.com>
2022-11-21 22:18:10 +0530

    added web3 connectivity

--- src/fragments/BannerTicket.jsx
+++ src/fragments/BannerTicket.jsx
@@ -1,22 +1,16 @@
 import React from "react";
+import { prizePOOL } from "../connection";
 
 const BannerTicket = () => {
   return (
     <React.Fragment>
       <div className="hidden lg:block bg-ticket w-[42rem] h-80 bg-center">
         <div className="text-white text-right mt-[4.5rem] pr-[4.8rem]">
-          <p className="font-normal text-lg">NEXT DRAW</p>
-          <p className="font-semibold text-4xl mt-2">
-            <span className="font-bold text-[3.5rem] mr-2">03</span>d{" "}
-            <span className="ml-2 font-bold text-[3.5rem] mr-2">10</span>h{" "}
-            <span className="ml-2 selection:font-bold text-[3.5rem] mr-2">
-              05
-            </span>
-            m
-          </p>
           <p className="font-normal text-lg mt-6">CURRENT PRIZE POOL</p>
           <p className="font-bold text-[3.5rem] tracking-wide mt-2">
-            $150,000.00
+          {console.log(prizePOOL)}
+            {prizePOOL>0 && "$"+prizePOOL}
+            {prizePOOL===0 && "$0"}
           </p>
         </div>
       </div>
